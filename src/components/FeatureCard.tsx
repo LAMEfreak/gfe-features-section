310c9c07cb87da3a46eb058158f8a5cc41a4e2c4
LAMEfreak <leeweirong@outlook.com>
2024-05-30 02:49:32 +0800

    Increase icon shadow

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -13,7 +13,7 @@ const FeatureCard: React.FC<FeatureCardProps> = ({
 }): React.JSX.Element => {
   return (
     <div className="flex flex-col justify-center items-center text-center">
-      <div className="text-indigo-700 p-3 shadow rounded-full mb-5 size-12">
+      <div className="text-indigo-700 p-3 shadow-md rounded-full mb-5 size-12">
         <Icon className="size-6"/>
       </div>
       <p className="font-semibold text-xl mb-2">{title}</p>
