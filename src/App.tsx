7c3cf76ed672df28b54aaf2963b34427b121f074
LAMEfreak <leeweirong@outlook.com>
2024-05-30 02:39:15 +0800

    Resize icons

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import FeatureGrid from "./components/FeatureGrid";
 
 function App() {
   return (
-    <section className="flex flex-col py-12 px-3 md:py-16 md:px-4 xl:p-24">
+    <section className="flex flex-col py-12 px-3 md:py-16 md:px-4 xl:py-24 xl:px-20">
       <div className="text-center mb-12 md:mb-16 xl:px-40">
         <p className="text-base font-semibold text-indigo-700 mb-3">
           Premium abstract images
